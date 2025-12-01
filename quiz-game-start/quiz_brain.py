e71fd7da322bd836504a3a1dba43126026c83aa7
Garth Hyrum Marcaida <20220026769@my.xu.edu.ph>
2025-12-01 20:37:48 +0800

    Going to go through this OOP tutorial again soon

---
+++ quiz-game-start/quiz_brain.py
@@ -0,0 +1,10 @@
+from main import question_bank
+
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_list = q_list
+        self.question_number = 0
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        input(f"Q {current_question[self.question_number]} {current_question.text} True/False? ")
