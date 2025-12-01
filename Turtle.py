33f899fa0e2cd4c4624e864d04237d3a29a3ff72
Garth Hyrum Marcaida <20220026769@my.xu.edu.ph>
2025-12-01 15:55:29 +0800

    OOP Fundamentals. Attributes and Methods

---
+++ Turtle.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+screen = Screen()
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("royalblue")
+timmy.speed(4)
+timmy.forward(100)
+timmy.penup()
+
+
+
+screen.exitonclick()
